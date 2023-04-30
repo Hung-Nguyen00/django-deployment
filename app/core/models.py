d5dea0f83bfa8ce610543e486d592fb3364202ac
Hung-Nguyen00 <thanhhung041100@gmail.com>
2023-04-30 12:29:17 +0700

    Add new title field

--- app/core/models.py
+++ app/core/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class Sample(models.Model):
     attachment = models.FileField()
+    title = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
